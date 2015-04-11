1128363fe6af2c142b64f5139a00b14b3f10085b
James Gatannah <james.gatannah@gmail.com>
2015-04-10 22:02:43 -0500

    Lots of debugging
    It all boiled down to forgetting that a component has to return a single
    element. The results of build-all don't cut it.
    
    This version at least draws my board...but it's getting mounted and then
    unmounted pretty much immediately. It also has that same warning about
    invalid checksums and the server rendering that's been giving me so many fits.

--- src/lg_checkers/core.cljs
+++ src/lg_checkers/core.cljs
@@ -5,6 +5,10 @@
 (enable-console-print!)
 (fw/start {:on-jsload (fn []
                         ;; This is what kicks off the magic
+                        ;; It doesn't seem to happen the first time through, though
+                        (print "Starting the system")
+                        ;; Q: Do I really want to restart the entire system every time
+                        ;; I save?
                         (system/ctor)
                         (print "System started successfully"))})
 
